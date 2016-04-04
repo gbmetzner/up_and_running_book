6284c1ea3432762f05602fd9b191af79e3dc623f
Gustavo Belloni Metzner <gbmetzner@yahoo.com>
2016-04-03 17:38:23 -0700

    Chapter 7 has been finished.

---
+++ chapter7/notesApp1-mocks.js
@@ -0,0 +1,10 @@
+angular.module('notesApp1Mocks', []) 
+	.factory('ItemService', [function() {
+		return {
+			list: function() {
+				return [
+						{id: 1, label: 'Mock'}
+					]; 
+			}
+		}; 
+}]);
